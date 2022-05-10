52c44e314c19f9de49e52add14b864b234e2fcee
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-10 17:50:12 +0400

    refactoring

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-let city = 'Samara';
 const inputSearch = document.querySelector('.input__search');
 const cityNameBlock = document.querySelector('.title__weather');
 const tempBlock = document.querySelector('.weather__temp');
 const timeBlock = document.querySelector('.weather__time');
 const descriptionBlock = document.querySelector('.weather__description');
 const weatherBlock = document.querySelector('.weather__wind');
+let city;
 
 document.addEventListener('keydown', (e) => {
    if (e.key === 'Enter') {
-      let value = inputSearch.value;
+      const value = inputSearch.value;
       if (!value) false;
       city = value;
-      init();
+      weatherDisplay(city);
       inputSearch.value = '';
    }
 });
 
-function init() {
+function weatherDisplay(city = 'Samara') {
    fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4a8f1c47ed68b7a3b449d82e5389b028`
    )
@@ -26,30 +26,31 @@ function init() {
 
          cityNameBlock.textContent = data.name;
 
-         tempBlock.innerHTML = `${temperature()}` + '&deg';
+         tempBlock.innerHTML = `${temperature()}&deg`;
 
          function temperature() {
-            let getTemp = data.main.temp;
-            let tempC = Math.floor(getTemp) - 273;
+            const getTemp = data.main.temp;
+            const tempC = Math.floor(getTemp) - 273;
             return tempC;
          }
-
-         const date = new Date();
-         timeBlock.textContent = `Local time: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
          descriptionBlock.textContent = data.weather[0].description;
 
          weatherBlock.textContent = `wind speed: ${data.wind.speed}`;
       })
-      .catch((e) => {
+      .catch(() => {
          alert('City is not found');
          city = 'Samara';
-         init();
+         weatherDisplay();
          inputSearch.value = '';
       });
 }
-init();
+weatherDisplay();
 
+const localTime = () => {
+   const date = new Date();
+   timeBlock.textContent = `Local time: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
 setInterval(() => {
-   init();
+   localTime();
 }, 1000);
